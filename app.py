a48f57dfe1668812c25fc29c0b316553aff7f373
sammydoodles <samarthk2402@gmail.com>
2022-05-07 16:47:01 +0100

    added python code for passcode change

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
@@ -17,8 +17,27 @@ def faces():
 
 @app.route("/passcode")
 def passcode():
-    passcode = 12345
-    return render_template("passcodes.html", passcode=passcode)
+    return render_template("passcodes.html")
+
+@app.route("/change_code")
+def change_code():
+    passcode = "12345"
+    old_code = request.args.get("curr_code", "")
+    new_code = request.args.get("new_code", "")
+    message = ""
+
+    if old_code == passcode:
+        try:
+            new_code = int(new_code)
+            passcode = str(new_code)
+            message = "Your passcode was successfully updated!"
+        except:
+            message = "Sorry, passcodes can only consist of numbers."
+    else:
+        message = "Sorry, incorrect passcode."
+
+    return render_template("change_code_result.html", message=message)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
