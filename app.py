48e85e9d8b620417f26984e05b0145503f2e95e6
sammydoodles <samarthk2402@gmail.com>
2022-05-08 12:47:06 +0100

    Added login page

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
+def login():
+    with open("passcode_data.txt", "r") as f:
+        passcode = f.read()
+
+    if request.method == "POST":
+        check_code = request.form["passcode"]
+        if passcode == check_code:
+            return redirect(url_for("home"))
+        else:
+            return render_template("login.html", incorrect=True)
+    else:
+        return render_template("login.html", incorrect=False)
+
+@app.route("/home")
 def home():
     return render_template("index.html")
 
@@ -19,15 +33,14 @@ def faces():
 def passcode():
     return render_template("passcodes.html")
 
-@app.route("/change_code")
+@app.route("/change_code", methods=["GET", "POST"])
 def change_code():
     with open("passcode_data.txt", "r") as f:
         passcode = f.read()
-    old_code = request.args.get("curr_code", "")
-    new_code = request.args.get("new_code", "")
     message = ""
 
-    if old_code == passcode:
+    if request.method == "POST":
+        new_code = request.form["new_code"]
         try:
             new_code = int(new_code)
             with open("passcode_data.txt", "w") as f:
@@ -35,10 +48,10 @@ def change_code():
             message = "Your passcode was successfully updated!"
         except:
             message = "Sorry, passcodes can only consist of numbers."
-    else:
-        message = "Sorry, incorrect passcode."
 
-    return render_template("change_code_result.html", message=message)
+        return render_template("change_code_result.html", message=message)
+    else:
+        return render_template("change_code_result.html", message=message)
 
 
 if __name__ == '__main__':
